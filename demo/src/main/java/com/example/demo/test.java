d286bc304bd3ab4b1ea82bc9c0f875f8daa0fb18
chenjing <jingchen0203@163.com>
2026-02-01 16:57:59 +0800

    "Hello World2"

--- demo/src/main/java/com/example/demo/test.java
+++ demo/src/main/java/com/example/demo/test.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World1");
+        System.out.println("Hello World2");
     }
 }
